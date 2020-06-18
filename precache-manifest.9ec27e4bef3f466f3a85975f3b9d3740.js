9ebc7effeed35abb5bca25fbea4c5b70c8ef56b3
aleksey-10 <alexcare1337@gmail.com>
2020-06-18 11:41:23 +0300

    Updates

---
+++ precache-manifest.9ec27e4bef3f466f3a85975f3b9d3740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223efcc5e95063158e901cc3929aec42",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "eb3e4d4b290657af403b",
+    "url": "/react-example/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "170ae003f0f6e603dee4",
+    "url": "/react-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb3e4d4b290657af403b",
+    "url": "/react-example/static/js/2.170ba0ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.170ba0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170ae003f0f6e603dee4",
+    "url": "/react-example/static/js/main.143b6542.chunk.js"
+  },
+  {
+    "revision": "f92f97a12c7e4802f575",
+    "url": "/react-example/static/js/runtime-main.7e491b2a.js"
+  }
+]);
